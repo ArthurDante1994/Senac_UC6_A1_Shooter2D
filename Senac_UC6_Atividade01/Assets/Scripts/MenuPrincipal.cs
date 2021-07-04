99cfe30c12fd2abc4a13ca702acbfc4b7c5aff40
ArthurDante1994 <74523524+ArthurDante1994@users.noreply.github.com>
2021-07-04 08:37:57 -0300

    Primeiro Commit AT03
     Neste momento estão feitos o menu principal do jogo e o menu de vitória do jogo.

---
+++ Senac_UC6_Atividade01/Assets/Scripts/MenuPrincipal.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuPrincipal : MonoBehaviour
+{
+    public GameObject menuprincipal;
+    public GameObject menuecredits;
+    // Start is called before the first frame update
+    void Start()
+    {
+        menuecredits.SetActive(false);
+        menuprincipal.SetActive(true);
+    }
+    // executando o game nesta função
+    public void startgame()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void Menugame()
+    {
+        SceneManager.LoadScene(0);
+    }
+    // Mostando o menu de creditos do Game
+    public void Credistsgame()
+    {
+        menuecredits.SetActive(true);
+        menuprincipal.SetActive(false);
+    }
+    public void Retornagame()
+    {
+        menuecredits.SetActive(false);
+        menuprincipal.SetActive(true);
+    }
+    // Função para sair do Game
+    public void exitsgame()
+    {
+        Application.Quit();
+    }
+}
---
+++ Senac_UC6_Atividade01/Assets/Scripts/MenuVitoria.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MenuVitoria : MonoBehaviour
+{
+    public void startgame()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void Menugame()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
