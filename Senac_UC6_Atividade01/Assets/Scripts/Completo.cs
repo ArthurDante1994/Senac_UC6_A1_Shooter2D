dac38f66a58c4afa13d8d882f65ee406a1f0150b
ArthurDante1994 <74523524+ArthurDante1994@users.noreply.github.com>
2021-07-04 10:40:53 -0300

    Commit Final AT03
    Condição de vitória colocada no jogo.

---
+++ Senac_UC6_Atividade01/Assets/Scripts/Completo.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Completo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Esat funcionado o sistema de saida!!");
+            SceneManager.LoadScene(2);
+        }
+    }
+}
